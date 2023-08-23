ce72a57b7b2a6f9dac23e0244e9a23c93117973f
Kasia Szewczyk <fisznikove@gmail.com>
2023-08-23 21:22:59 +0200

    arrangement the code

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,19 +1,20 @@
 import "./style.css";
 
 const Buttons = ({tasks, hideDoneTask}) => (
-    
         <div className="section__buttons">
             {tasks.length>0 && (
               <>
               <button className= "header__buttons">
               {hideDoneTask ? "Pokaż ukończone" : "Ukryj ukończone"}
               </button>
-              <button className= "header__buttons tasks__button--hiddenAllDone"
-              disabled= {tasks.every(({ done })=> done)}> Ukończ wszystkie
+              <button 
+              className= "header__buttons tasks__button--hiddenAllDone"
+              disabled= {tasks.every(({ done })=> done)}
+              > 
+              Ukończ wszystkie
               </button>`
               </>
             )}
         </div>
-    
 );
 export default Buttons;
